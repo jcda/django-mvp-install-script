e6f33794f132fe12527d6d9a15917b5f68177b01
jcda <jcda@roo.red>
2015-03-27 20:52:09 +0100

    adding a skeleton for the installation script

---
+++ django-mvp-install.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+###########################################################
+#   Installation script for  the full framework           #
+#                                                         #
+# steps followed :                                        #
+#                                                         #
+# 1: Declaration of the environment variables             #
+#                                                         #
+# 2: installation of the desired packages                 #
+#                                                         #
+# 3: installation of the python virtual environment       #
+#      a. installation of pip                             #
+#      b. installation of django                          #
+#      c. installation of uwsgi                           #
+#                                                         #
+# 4: installation of django then the edge framework       #
+#      a. installation of the packages                    #
+#      b. migration of the sqlite database                #
+#      c. creation of a superuser account                 #
+#                                                         #
+# 5: installation of nginx                                #
+#                                                         #
+###########################################################
+
+###########################################################
+# 1: Declaration of the environment variables             #
+###########################################################
+
+WORK_DIRECTORY =
+PROJECT_NAME =
+EDGE_URL =
+PIP_URL =
+
+###########################################################
+# 2: installation of the desired packages                 #
+###########################################################
+
+
+###########################################################
+# 3: installation of the python virtual environment       #
+###########################################################
+
+
+###########################################################
+#    3.a. installation of pip                             #
+###########################################################
+
+
+###########################################################
+#    3.b. installation of django                          #
+###########################################################
+
+
+###########################################################
+#    3.c. installation of uwsgi                           #
+###########################################################
+
+
+###########################################################
+# 4: installation of django then the edge framework       #
+###########################################################
+
+
+###########################################################
+#    4.a. installation of the packages                    #
+###########################################################
+
+
+###########################################################
+#    4.b. migration of the sqlite database                #
+###########################################################
+
+
+###########################################################
+#    4.c. creation of a superuser account                 #
+###########################################################
+
+
+###########################################################
+# 5: installation of nginx                                #
+###########################################################
